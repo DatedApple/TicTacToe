f56165509aa95e096e488374c62a62c1c1a0e0ba
DatedLinux <diego.who2005@gmail.com>
2025-06-03 17:29:50 -0600

    Created a program to store the logic behind the bot, modified main.py to allow for two players and cleaned most functions to make them easier to read.

---
+++ AI/minmax.py
@@ -0,0 +1,19 @@
+import copy
+import board
+
+def bestMove(board, current):
+    bestScore = int(-1e9)
+    placement = [-1, -1]
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == " ":
+                board[i][j] = current
+                score = minmax(board, current)
+                if score > bestScore:
+                    bestScore = score
+                    placement = [i, j]
+                board[i][j] = " "
+    return placement[0] * 3 + placement[1]
+
+def minmax(board, curr):
+    return int(-1e9)
